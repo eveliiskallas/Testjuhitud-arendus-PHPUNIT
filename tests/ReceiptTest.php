81f1ce6b1860e11af2e5691e5070dfa92b9d3b21
Eveliis <eveliis.kallas@khk.ee>
2019-04-08 10:43:06 +0300

    02 Extend a unit test 01_07

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -6,12 +6,29 @@ use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 class ReceiptTest extends TestCase {
+    public function setUp() {  //lisame meetodi setUp
+        $this->Receipt = new Receipt(); // loome receipt objekti, mis on võrdne uue receipt objektiga
+    }
+    public function tearDown() {  //lisame meetodi tearDown, see tühistab receipt objekti, tagades, et PHP ei kannaks midagi ühest testikäigust teise.
+        unset($this->Receipt); //
+    }
     public function testTotal() {
-        $Receipt = new Receipt();
+        $input = [0,2,5,8]; // antud arvud, mis liidetakse kokku
+        $output = $this->Receipt->total($input); // output on siis võrdne selle arve summaga. See muudab testi rohkem isoleeritumaks
         $this->assertEquals(
-            15,
-            $Receipt->total([0,2,5,8]),
-            'When summing the total should equal 15'
+            15, // eeldatakse, et arve summa on 15
+            $output, // väljastab summa
+            'When summing the total should equal 15' // väljastab sõnumi, kui test kukub läbi
         );
     }
 }
+
+//    public function testTotal() {
+////        $Receipt = new Receipt();
+////        $this->assertEquals(
+////            15,
+////            $Receipt->total([0,2,5,8]),
+////            'When summing the total should equal 15'
+////        );
+////    }
+////}
